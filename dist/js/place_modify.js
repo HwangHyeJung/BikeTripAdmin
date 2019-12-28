3de174c57abb2b818ef167303333646f8db1a36c
michelle <hwanghyejung001@gmail.com>
2019-12-28 18:22:12 +0900

    0.8

--- dist/js/place_modify.js
+++ dist/js/place_modify.js
@@ -1,30 +1,8 @@
 $(document).ready(function (e) {
     // 모달 박스 가져오기
-    $("#spot-modal").load("modal/spot_modal.html");
     $("#path-modal").load("modal/path_modal.html");
-
-    // 카테고리 플러스 버튼
-    $("#plus_btn").click(function () {
-        var category_val = '<div class="form-row"><div class="form-group col-md-6">';
-        category_val += '<select id="category1" class="form-control" name="category1">';
-        category_val += '<option value="">선택</option>';
-        category_val += '<option value="load">로드</option>';
-        category_val += '<option value="mountainLoad">산악</option>';
-        category_val += '<option value="sea">바다</option>';
-        category_val += '<option value="mountain">산</option>';
-        category_val += '<option value="river">강변</option>';
-        category_val += '</select></div>';
-        category_val += '<div class="form-group col-md-6">';
-        category_val += '<i class="icon-danger cil-x-circle align-middle x-btn" style="cursor: pointer;"></i>';
-        category_val += '</div></div>';
-
-        $("#category_list").append(category_val);
-    });
-
-    // 카테고리 삭제
-    $("#category_list").on("click", ".x-btn", function () {
-        $(this).parent().parent().remove();
-    });
+    $("#travel-modal").load("modal/travel_modal.html");
+    // $("#api-modal").load("modal/api_modal.html");
 
     // 경로 등록 버튼
     $("#path_add").click(function () {
@@ -45,22 +23,22 @@ $(document).ready(function (e) {
         }
     });
 
-    // 장소 등록 버튼
-    $("#place_add").click(function () {
-        var place_val = $("#place").val();
-        if (place_val != "") {
-            var place_list = '<div class="form-group row">';
-            place_list += '<div class="col-md-1 align-self-center text-right"></div>';
-            place_list += '<div class="col-md-11">';
-            place_list += '<input type="radio" name="place_num" id="place_num" class="mr-3">';
-            place_list += place_val
-            place_list += '</div></div>';
+    // 여행지 등록 버튼
+    $("#travel_add").click(function () {
+        var travel_val = $("#travel").val();
+        if (travel_val != "") {
+            var travel_list = '<div class="form-group row">';
+            travel_list += '<div class="col-md-1 align-self-center text-right"></div>';
+            travel_list += '<div class="col-md-11">';
+            travel_list += '<input type="radio" name="travel_num" id="travel_num" class="mr-3">';
+            travel_list += travel_val
+            travel_list += '</div></div>';
 
-            $("#place_list").append(place_list);
-            $("#place").val("");
+            $("#travel_list").append(travel_list);
+            $("#travel").val("");
         }
         else {
-            alert("장소를 검색해 주세요!");
+            alert("여행지를 검색해 주세요!");
         }
     });
 
@@ -79,17 +57,17 @@ $(document).ready(function (e) {
             node.remove();
         })
     });
-    // 장소 UP, DOWN, DEL 버튼
-    $("#place_up").click(function () {
-        var node = $('input[name="place_num"]:checked').parent().parent();
+    // 여행지 UP, DOWN, DEL 버튼
+    $("#travel_up").click(function () {
+        var node = $('input[name="travel_num"]:checked').parent().parent();
         moveUp(node);
     });
-    $("#place_down").click(function () {
-        var node = $('input[name="place_num"]:checked').parent().parent();
+    $("#travel_down").click(function () {
+        var node = $('input[name="travel_num"]:checked').parent().parent();
         moveDown(node);
     });
-    $("#place_del").click(function () {
-        var node = $('input[name="place_num"]:checked').parent().parent();
+    $("#travel_del").click(function () {
+        var node = $('input[name="travel_num"]:checked').parent().parent();
         node.fadeOut(function () {
             node.remove();
         })
@@ -102,10 +80,8 @@ $(document).ready(function (e) {
         line.insertAfter(line.next());
     }
 
+    // 이미지 파일
     $("input[type='file']").change(function (e) {
-
-        //div 내용 비워주기
-        $('#preview').empty();
 
         var files = e.target.files;
         var arr = Array.prototype.slice.call(files);
@@ -120,7 +96,7 @@ $(document).ready(function (e) {
         preview(arr);
 
 
-    });//file change
+    });
 
     function checkExtension(fileName, fileSize) {
 
@@ -129,13 +105,13 @@ $(document).ready(function (e) {
 
         if (fileSize >= maxSize) {
             alert('파일 사이즈 초과');
-            $("input[type='file']").val("");  //파일 초기화
+            // $("input[type='file']").val("");  //파일 초기화
             return false;
         }
 
         if (regex.test(fileName)) {
             alert('업로드 불가능한 파일이 있습니다.');
-            $("input[type='file']").val("");  //파일 초기화
+            // $("input[type='file']").val("");  //파일 초기화
             return false;
         }
         return true;
@@ -144,7 +120,7 @@ $(document).ready(function (e) {
     function preview(arr) {
         arr.forEach(function (f) {
 
-            var img_html = '<div class="card" style="width: 40rem;">';
+            var img_html = '<div class="card" style="width: 18rem;">';
 
             //이미지 파일 미리보기
             if (f.type.match('image.*')) {
@@ -154,9 +130,11 @@ $(document).ready(function (e) {
                     img_html += '<img src="' + e.target.result + '" class="card-img-top img-thumbnail">';
                     img_html += '<div class="card-body">';
                     img_html += '<p class="card-text">' + f.name + '</p>';
-                    img_html += '</div></div>';
+                    img_html += '<p class="card-text text-right">';
+                    img_html += '<button type="button" class="btn btn-outline-danger x-btn lm-2">삭제</button>';
+                    img_html += '</p></div></div>';
 
-                    $(img_html).appendTo('#preview');
+                    $(img_html).appendTo('#img_list');
                 }
                 reader.readAsDataURL(f);
             } else {
@@ -165,25 +143,27 @@ $(document).ready(function (e) {
         });//arr.forEach
     }
 
+    // 이미지 삭제
+    $("#img_list").on("click", ".x-btn", function () {
+        $(this).parent().parent().parent().remove();
+    });
 });
 
 $('#travelForm').validate({
     rules: {
-        title: 'required',
-        summary1: 'required',
-        summary2: 'required',
-        imageFile: 'required',
-        category1: 'required',
-        tag: 'required',
+        shop: 'required',
+        category: 'required',
+        open: 'required',
+        tel: 'required',
+        facility: 'required',
         location: 'required'
     },
     messages: {
-        title: '제목을 입력해 주세요!',
-        summary1: '한줄요약을 입력해 주세요!',
-        summary2: '요약을 입력해 주세요!',
-        imageFile: '이미지파일을 선택해 주세요.',
-        category1: '카테고리는 하나이상 선택해 주세요.',
-        tag: '태그를 입력해 주세요',
+        shop: '상호명을 입력해 주세요!',
+        category: '카테고리를 선택해 주세요!',
+        open: '운영시간을 입력해 주세요!',
+        tel: '전화번호를 입력해 주세요!',
+        facility: '편의시설을 입력해 주세요!',
         location: '위치를 선택해 주세요.'
     },
     errorElement: 'em',
