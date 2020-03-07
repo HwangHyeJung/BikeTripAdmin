a1b130907e674962e99c9d29d32a9fd14eed20ea
michelle <hwanghyejung001@gmail.com>
2020-03-07 23:44:22 +0900

    original

--- dist/js/using_map.js
+++
@@ -1,9 +0,0 @@
-function initMap() {
-    // The location of Uluru
-    var uluru = { lat: 37.4669621, lng: 126.8874035 };
-    // The map, centered at Ulur
-    var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 11, center: uluru });
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({ position: uluru, map: map });
-}
--- dist/js/validation.js
+++ dist/js/validation.js
@@ -1,3 +1,4 @@
+import $ from 'jquery';
 /**
  * --------------------------------------------------------------------------
  * CoreUI Pro Boostrap Admin Template (3.0.0-beta.0): validation.js
